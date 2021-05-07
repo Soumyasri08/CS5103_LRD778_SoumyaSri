9df61879b21905c6d3d1ac1cfa24c2c619db1d34
Soumya Sri <70061578+Soumyasri08@users.noreply.github.com>
2021-05-07 10:40:27 +0530

    Update Project  Word_Statistics.java

--- Project  Word_Statistics.java
+++ Project  Word_Statistics.java
@@ -2,71 +2,125 @@
 import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Count_Words {
+public class WordStatistics {
+
+	Map<String, Integer> wordCounts = new HashMap<String, Integer>();
+	static StringBuilder sb = new StringBuilder();
+
+	static Scanner sc = new Scanner(System.in);
+	static int characterCount = 0;
+
+	public Map<String, Integer> counter(String words[]) throws IOException {
+
+		for (String word : words) {
+			// The length of the word gives the count of characters
+
+			characterCount += word.length();
+
+			// I am assuming all the characters are in lower case.
+
+			word = word.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+
+			// If the word occurs for the first time then we consider the count as 1
+			if (!wordCounts.containsKey(word)) {
+				wordCounts.put(word, 1);
+
+			} else {
+				wordCounts.put(word, wordCounts.get(word) + 1); // If the word occurs already then we increment its
+																// count
+			}
+
+		}
+		
+		return wordCounts;
+
+	}
+
+	public String replaceWord(String words[], String toBeReplaced, String toBeReplacedBy) {
+
+		for (String word : words) {
+			word = word.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+			if (word.equals(toBeReplaced)) {
+				 sb.append(toBeReplacedBy).append(" ").toString();
+
+			} else {
+				 sb.append(word).append(" ").toString();
+
+			}
+
+		}
+       
+		sb.append(System.getProperty("line.separator"));
+		return sb.toString();
+	}
+
+	private void writeFile(String fileName, String finalResponse) throws IOException {
+		FileWriter writer = new FileWriter(fileName);
+		writer.write(finalResponse);
+		writer.close();
+
+		BufferedReader reader = new BufferedReader(new FileReader(fileName));
+		String text = "";
+		while ((text = reader.readLine()) != null) {
+			System.out.println(text);
+		}
+		
+		reader.close();
+	}
+	
+	
 
 	public static void main(String[] args) throws IOException {
+		
+		WordStatistics wordStatistics = new WordStatistics();
+		wordStatistics.processFile();
+		
+	}
 
-		// We use the BufferedReader because it always checks whether the requested data
-		// already exist.
+	private void processFile() {
 		BufferedReader reader;
-
-		// Initialize lineCount and characterCount to Zero
-		int lineCount = 0, characterCount = 0;
-
-		// We use Map for key-value association
-		Map<String, Integer> wordCounts = new HashMap<String, Integer>();
 		try {
-			Scanner sc = new Scanner(System.in);
+			// Scanner sc = new Scanner(System.in);
 			System.out.print("name of the text file : ");
 			String fileName = sc.nextLine();
+			
 			reader = new BufferedReader(new FileReader(fileName)); // reads the file
-			String line;
-
+			String line = " ";
+			int lineCount = 0;
+			String words[] = null;
+			System.out.println("enter the word which is to be replaced");
+			String toBeReplaced = sc.next();
+			System.out.println("enter the word which is to be replaced by");
+			String toBeReplacedBy = sc.next();
+			
 			while ((line = reader.readLine()) != null) {
-				// If the file contains the text and is not empty
 
-				lineCount++; // Increment the lineCount
-
-				// split the line into words and store it in array
-				String words[] = line.trim().split("\\s+");
-
-				for (String word : words) {
-					// The length of the word gives the count of characters
-
-					characterCount += word.length();
-                    
-					//I am assuming all the characters should be converted to lower case. 
-					//So that we can increment the count frequency of same word even if it is in upper case
-					word = word.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
-
-					// If the word occurs for the first time then we consider the count as 1
-					if (!wordCounts.containsKey(word)) {
-						wordCounts.put(word, 1);
-
-					}
-
-					// If the word occurs already then we increment its count
-					else {
-						wordCounts.put(word, wordCounts.get(word) + 1);
-					}
-				}
+				lineCount++;
+				words = line.trim().split("\\s+");
+				counter(words);
+				replaceWord(words, toBeReplaced, toBeReplacedBy);
 			}
-			reader.close();
+			String finalResponse = sb.toString();
+		
+			System.out.println("linecount :" + lineCount);
+			System.out.println("Total character count :" + characterCount);
+			System.out.println("\n ***Word count***");
+
+			for (String Word : wordCounts.keySet()) {
+				System.out.println(Word + "\t" + wordCounts.get(Word)); // prints the word along with its count
+			}
+			System.out.println("***File Content After Replacement***\n");
+			writeFile(fileName, finalResponse);
 			sc.close();
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println("Line count :" + lineCount); // prints line count
-		System.out.println("character count :" + characterCount);// prints character count
-		System.out.println("\n Word  count");
 
-		for (String word : wordCounts.keySet())
-
-			System.out.println(word + "\t" + wordCounts.get(word)); // prints the word along with its count
-
+		
 	}
 }
